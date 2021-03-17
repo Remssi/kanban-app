8236543726249434749d3bfab1247339fc8392e6
Remssi <Zjanne95@gmail.com>
2021-03-17 14:14:08 +0200

    Create interface for Task and enum for Task status

--- src/consts.tsx
+++ src/consts.tsx
@@ -2,9 +2,23 @@ export const DragItemTypes = {
   Card: "card"
 }
 
+export interface Task {
+  id: number
+  title: string
+  bodyText?: string
+  taskType: TaskType
+  status: TaskStatus
+}
+
 export enum TaskType {
   Bug = "bug",
   Feature = "feature",
   Chore = "chore",
   Documentation = "documentation"
+}
+
+export enum TaskStatus {
+  Backlog = "backlog",
+  ToDo = "toDo",
+  InProgress = "inProgress"
 }
